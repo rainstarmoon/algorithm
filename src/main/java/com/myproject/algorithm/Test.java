76411b6b9c6e0448df3eb71e3c87488d8b59e9d0
xiazeyu <lil>
2019-11-10 00:37:12 +0800

    1

--- src/main/java/com/myproject/algorithm/Test.java
+++ src/main/java/com/myproject/algorithm/Test.java
@@ -4,11 +4,24 @@ import java.util.PriorityQueue;
 
 public class Test {
 
-    public static void main(String[] args) {
+    //public static void main(String[] args) {
 
         //PriorityQueue queue = new PriorityQueue();
 
-        System.out.println("\u000C");
+        //System.out.println("\u000C");
+    //}
+	
+	public void test() {
+        int i = 8;
+        while ((i -= 3) > 0) ;
+        System.out.println("i = " + i);
+    }
+
+    public static void main(String[] args) {
+        Test test = new Test();
+        for (int i = 0; i < 5_0000; i++) {
+            test.test();
+        }
     }
 
 
