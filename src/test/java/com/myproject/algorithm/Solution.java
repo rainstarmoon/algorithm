eac882ca8b1453422c66f2f7dbabfe5b7bbf7f4b
xiazeyu <lil>
2020-01-17 16:06:12 +0800

    1

--- src/test/java/com/myproject/algorithm/Solution.java
+++ src/test/java/com/myproject/algorithm/Solution.java
@@ -1,28 +1,5 @@
 package com.myproject.algorithm;
 
 public class Solution {
-    private A a =new A(1);
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        A a = solution.getA();
-        System.out.println(a);
-        a= new A(2);
-        System.out.println(solution.getA());
-    }
-    public A getA() {
-        return a;
-    }
 }
-class A{
-    int a;
-    public A(int a){
-        this.a = a;
-    }
 
-    @Override
-    public String toString() {
-        return "A{" +
-                "a=" + a +
-                '}';
-    }
-}
