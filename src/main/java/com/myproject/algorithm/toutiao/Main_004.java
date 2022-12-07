92e811167f71005d094f069fcd2e7e40a46bfdfb
zeyu.xia <1215884463@qq.com>
2022-12-07 15:21:49 +0800

    Default Changelist

---
+++ src/main/java/com/myproject/algorithm/toutiao/Main_004.java
@@ -0,0 +1,20 @@
+package com.myproject.algorithm.toutiao;
+
+/**
+ * 输入 十个1-10的数 求和大于90的概率
+ */
+public class Main_004 {
+
+    public static void main(String[] args) {
+        int[][] arr = {
+                {1, 2, 3, 4, 5, 6, 7, 8, 9, 10},
+                {0, 0, 1, 1, 1, 1},
+                {0, 0, 1, 1, 1, 1},
+                {0, 1, 0, 0, 1, 1},
+                {1, 1, 0, 0, 1, 1}
+        };
+
+
+    }
+
+}
